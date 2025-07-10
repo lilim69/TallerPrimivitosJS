8e3dc5ac540305defbafa9635766d90c52c462da
lilim69 <mariaca.rayodu@ecci.edu.co>
2025-07-10 16:19:01 -0500

    Ejercicio 1 terminado

---
+++ Ejercicio1.js
@@ -0,0 +1,10 @@
+const prompt = require("prompt-sync")();
+
+let nombre = "Camila";
+let edad = 22;
+let esEstudiante = true;
+
+console.log("Hola", nombre);
+console.log("tienes", edad, "años");
+console.log("¿Eres estudiante?", esEstudiante);
+
